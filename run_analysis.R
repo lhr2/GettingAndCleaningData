b244195caebbe307124b4ce679a0d13ef8e94b96
lhr2 <lrepicky@gmail.com>
2015-01-25 17:01:28 -0500

    Update run_analysis

--- run_analysis.R
+++ run_analysis.R
@@ -6,7 +6,8 @@
 #2. Extracts only the measurements on the mean and standard deviation for each measurement. 
 #3. Uses descriptive activity names to name the activities in the data set
 #4. Appropriately labels the data set with descriptive variable names. 
-#5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+#5. From the data set in step 4, creates a second, independent tidy data set with the average 
+#of each variable for each activity and each subject.
 
 #set working directory to where data files are stored
 setwd('/Users/lisar/Documents/R/GettingAndCleaningData/Course Project/UCI HAR Dataset')
@@ -30,4 +31,58 @@ activity_labels <- read.table("activity_labels.txt")
 features <- read.table("features.txt")
 
 #add column names to the datasets
-colnames(activity_labels) <- c('activityId', 'activityType')
+colnames(x_test) <- t(features[2])
+colnames(y_test) <- "activityId"
+colnames(subject_test) <- "subjectId"
+colnames(x_train) <- t(features[2])
+colnames(y_train) <- "activityId"
+colnames(subject_train) <- "subjectId"
+colnames(activity_labels) <- c("activityId", "activityName")
+
+#merge the x and y datasets
+testdata <- cbind(y_test, subject_test, x_test)
+traindata <- cbind(y_train, subject_train, x_train)
+
+#merge the test and training datasets
+data <- rbind(traindata, testdata)
+
+#make logical vector to be true for only desired columns
+#looking for mean and standard deviation
+log <- (grepl("activity..", colnames(data)) | grepl("subject..", colnames(data)) |
+                grepl("..-mean..", colnames(data)) & !grepl("..-meanFreq..", colnames(data)) |
+                grepl("-std..", colnames(data)))
+
+#pull only columns with logical vector value = TRUE
+data <- data[log == TRUE]
+
+#add activity names by merging activity_labels dataset with the data dataset
+data <- merge(data, activity_labels, by = 'activityId', all.x = TRUE)
+
+#create a vector of column names so they are more easily updated
+columns <- colnames(data)
+
+#clean up column names
+for (i in 1:length(columns)) {
+        columns[i] <- gsub("-mean()", "Mean", columns[i])
+        columns[i] <- gsub("-std()", "StdDev", columns[i])
+        columns[i] <- gsub("^(t)", "time", columns[i])
+        columns[i] <- gsub("^(f)", "frequency", columns[i])
+        columns[i] <- gsub("BodyBody", "Body", columns[i])
+        columns[i] <- gsub("\\()", "", columns[i])
+}
+
+#update the column names
+colnames(data) <- columns
+
+#remove activity name to simplify process of creating tidy dataset
+data_ <- data[, names(data) != 'activityName']
+
+#create a tidy dataset that summarizes the data dataset to just have the average of each
+#variable for each activity and each subject
+tidy <- aggregate(data_[,names(data_) != c('activityId', 'subjectId')], 
+                  by = list(activityId = data_$activityId, subjectId = data_$subjectId), mean)
+
+#add the acitivity name back to the dataset
+tidy <- merge(tidy, activity_labels, by = 'activityId', all.x = TRUE)
+
+write.table(tidy, './tidyData.txt', row.names = TRUE, sep = '\t')
